56d3bb36ed60e1fe8f4037d89f2318eb5607b274
Paul Budu <paul.budu@tecknoworks.com>
2017-07-03 14:42:30 +0300

    Initial MVC project

---
+++ Labyrinth/Labyrinth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Labyrinth.Startup))]
+namespace Labyrinth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
