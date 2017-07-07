3698c980aedb5c3171f1feb4df20db5cba36d48f
andreeapaula.major <andreeapaula.major@TKW525-PC>
2017-07-07 13:21:26 +0300

    +coins for second map

--- Labyrinth/Labyrinth/Content/Game/js/game.js
+++ Labyrinth/Labyrinth/Content/Game/js/game.js
@@ -12,7 +12,7 @@ var game = {
     // Run on page load.
     "onload" : function () {
         // Initialize the video.
-        if (!me.video.init(3000, 1800, {wrapper : "screen", scale : "auto", scaleMethod : "flex-width"})) {
+        if (!me.video.init(300, 300, {wrapper : "screen", scale : "auto", scaleMethod : "flex-width"})) {
             alert("Your browser does not support HTML5 canvas.");
             return;
         }
